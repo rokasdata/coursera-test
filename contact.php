cccc468c2bd3fbae7a4b2be1d5d6db0cdb412bed
rokasdata <renaldas.pocevicius@gmail.com>
2017-01-05 10:54:06 +0000

    Added.

--- contact.php
+++ contact.php
@@ -1,13 +1,13 @@
 <?php
 
-$from="noreply@gmail.com";
+$from="renaldas.pocevicius@gmail.com";
 $email="renaldas.pocevicius@gmail.com";
 $subject=$_POST['Subject'];
 $message=$_POST['Message'];
 
 mail ($email, $subject, $message, "From:".$from);
 
-
+Print "Your message has been sent";
 
 
 
