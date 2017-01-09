f558a46fd352f4be340bdd93d718228a89be1ecc
rokasdata <renaldas.pocevicius@gmail.com>
2017-01-09 11:54:55 +0000

    add

--- site/contact.php
+++ site/contact.php
@@ -27,3 +27,60 @@ mail( $to, $subject, $message, $headers)
 
 ?>
 
+<!DOCTYPE html>
+	<html>
+	<head>
+	<meta charset="utf-8">
+		<title>Hello Coursera</title>
+		<link rel="stylesheet" href="stylesheet/style.css" media="all" />
+	</head>
+	<body>
+		<div id="wraper">
+			<div id="content">
+				<ul class="list">
+					<li>
+						<a href="#">home</a>
+					</li>
+					<li>
+						<a href="#">about</a>
+					</li>
+					<li>
+						<a href="#">contact</a>
+					</li>
+				</ul>
+				<h2>Contacts</h2>
+				<p id="feedback"><?php $feedback; ?></p>
+				<form action="?" method="post">
+					<ul>
+						<li>
+							<label for="name">Name</label>
+							<input type="text" name="name" id="name"/>
+						</li>
+						<li>
+							<label for="email">email</label>
+							<input type="text" name="email" id="email"/>
+						</li>
+						<li>
+							<label for="topic">Topic:</label>
+							<select >
+								<option value="Young">Young</option>
+								<option value="Midlle age">Midlle-age</option>
+								<option value="Old">Old</option>
+							</select>
+
+						</li>
+						<li>
+							<label for="message">The story about you</label>
+							<textarea name="message" id="message" cols="42" rows="4"></textarea>
+						</li>
+						<li>
+							<input id="button" type="submit" value="send">
+						</li>
+					</ul>
+				</form>
+			</div>
+		</div>
+
+	</body>	
+
+	</html>	
